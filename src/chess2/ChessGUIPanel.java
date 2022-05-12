072a7863bb2966aa70c48cf1fbd222461d247703
SylvCrane <hng1110@autuni.ac.nz>
2022-05-12 18:16:23 +1200

    Update ChessGUIPanel.java

--- src/chess2/ChessGUIPanel.java
+++ src/chess2/ChessGUIPanel.java
@@ -4,6 +4,7 @@
  */
 package chess2;
 
+import java.awt.Image;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -11,10 +12,36 @@ import javax.swing.JPanel;
  *
  * @author navjo
  */
-public class ChessGUIPanel extends JFrame
+public class ChessGUIPanel extends JPanel
 {
-    private Square[][] board;
+    public Image image;
+    public JPanel panel;
     
+    public ChessGUIPanel(Image image, JPanel panel)
+    {
+        this.image = image;
+        this.panel = panel;
+    }
+    
+    public void setUpGUIBoard(ChessGUIPanel[][] GUIBoard, Square[][] board)
+    {
+        
+        for (int yIndex = 0; yIndex < 8; yIndex++)
+        {
+            for (int xIndex = 0; xIndex < 8; xIndex++)
+            {
+                GUIBoard[xIndex][yIndex] = new ChessGUIPanel(null, new JPanel());
+                
+                if (board[xIndex][yIndex].getPiece() != null)
+                {
+                    StringBuilder fileLocation = new StringBuilder();
+                    fileLocation.append("./resources/");
+                    fileLocation.append(board[xIndex][yIndex].getPiece().pieceName);
+                    fileLocation.append(".jpg");
+                }
+            }
+        }
+    }
     
     
     
