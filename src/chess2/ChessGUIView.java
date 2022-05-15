82756b32b36866112c374e37658ac043e81dc164
SylvCrane <hng1110@autuni.ac.nz>
2022-05-15 16:24:49 +1200

    Update ChessGUIView.java

--- src/chess2/ChessGUIView.java
+++ src/chess2/ChessGUIView.java
@@ -4,10 +4,33 @@
  */
 package chess2;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 /**
  *
  * @author navjo
  */
-public class ChessGUIView {
+public class ChessGUIView extends JPanel{
     
+    private ChessGUISquarePanel[][] GUIBoard;
+    private JButton jButton1;
+    private JButton jButton17;
+    private JButton jButton18;
+    private JButton jButton19;
+    private JButton jButton2;
+    private JButton jButton20;
+    private JButton jButton21;
+    private JButton jButton22;
+    private JButton jButton23;
+    private JButton jButton24;
+    private JButton jButton25;
+    private JButton jButton26;
+    private JButton jButton27;
+    private JButton jButton28;
+    private JButton jButton29;
+    private JButton jButton3;
+    private JButton jButton4;
+    
+            
 }
