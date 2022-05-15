020bacd7103e24e3faccb6bb253c837dbcb3e7bd
SylvCrane <hng1110@autuni.ac.nz>
2022-05-15 16:43:06 +1200

    Update Queen.java

--- src/Model/Queen.java
+++ src/Model/Queen.java
@@ -10,157 +10,33 @@ package Model;
  */
 public class Queen extends Piece{
     
-    public Queen(String pieceName, int potential_moves, Direction direction, PlayerColour colour)
+    public Queen(String pieceName, int potential_moves, PlayerColour colour)
     {
         super(pieceName);
         this.pieceName = pieceName;
         this.potential_moves = potential_moves;
-        this.direction = direction;
         this.colour = colour;
     }
 
     @Override
-    public int CheckMove(Piece piece, int x_direction, int y_direction, int potential_moves, Square[][] board) 
+    public int CheckMove(Piece piece, int x_direction, int y_direction, Square finalPosition, Square[][] board) 
     {
         int check = 1;
         
-       if (piece.direction == Direction.NORTH)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction][y_direction + i].getPiece() != null && board[x_direction][y_direction + i].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0;
-                   break;
-               }
-           }
-       }
-       if (piece.direction == Direction.NORTHEAST)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction + i][y_direction + i].getPiece() != null && board[x_direction + i][y_direction + i].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0;
-                   break;
-               }
-           }
-       }
-       if (piece.direction == Direction.EAST)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction + i][y_direction].getPiece() != null && board[x_direction + i][y_direction].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0; 
-                   break;
-               }
-           }
-       }
-       if (piece.direction == Direction.SOUTHEAST)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction + i][y_direction - i].getPiece() != null && board[x_direction + i][y_direction - i].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0;
-                   break;
-               }
-           }
-       }
-       if (piece.direction == Direction.SOUTH)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction][y_direction - i].getPiece() != null && board[x_direction][y_direction - i].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0;
-                   break;
-               }
-           }
-       }
-       if (piece.direction == Direction.SOUTHWEST)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction - i][y_direction - i].getPiece() != null && board[x_direction - i][y_direction - i].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0;
-                   break;
-               }
-           }
-       }
-       if (piece.direction == Direction.WEST)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction - i][y_direction].getPiece() != null && board[x_direction - i][y_direction].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0;
-                   break;
-               }
-           }
-       }
-       if (piece.direction == Direction.NORTHWEST)
-       {
-           for (int i = 1; i < potential_moves + 1; i++)
-           {
-               if (board[x_direction - i][y_direction + i].getPiece() != null && board[x_direction - i][y_direction + i].getPiece().colour == piece.colour)
-               {
-                   System.out.println("You cannot move the queen here");
-                   check = 0;
-                   break;
-               }
-           }
-       }
+       if (board[finalPosition.getX_location()][finalPosition.getY_location()].isValidLocation() == false)
+            {
+                check = 1;
+            }
        
        return check;
     }
 
     @Override
-    public void MakeMove(Piece piece, int x_direction, int y_direction, int potential_moves, Square[][] board) 
+    public void MakeMove(Piece piece, int x_direction, int y_direction, Square finalPosition, Square[][] board) 
     {
         
-        if (piece.direction == Direction.NORTH)
-        {
-            board[x_direction][y_direction + potential_moves].setPiece(piece);
-        }
-        else if (piece.direction == Direction.NORTHEAST)
-        {
-            board[x_direction + potential_moves][y_direction + potential_moves].setPiece(piece);
-        }
-        else if (piece.direction == Direction.EAST)
-        {
-            board[x_direction + potential_moves][y_direction].setPiece(piece);
-        }
-        else if (piece.direction == Direction.SOUTHEAST)
-        {
-            board[x_direction + potential_moves][y_direction - potential_moves].setPiece(piece);
-        }
-        else if (piece.direction == Direction.SOUTH)
-        {
-            board[x_direction][y_direction - potential_moves].setPiece(piece);
-        }
-        else if (piece.direction == Direction.SOUTHWEST)
-        {
-            board[x_direction - potential_moves][y_direction - potential_moves].setPiece(piece);
-        }
-        else if (piece.direction == Direction.WEST)
-        {
-            board[x_direction - potential_moves][y_direction].setPiece(piece);
-        }
-        else if (piece.direction == Direction.NORTHWEST)
-        {
-            board[x_direction - potential_moves][y_direction + potential_moves].setPiece(piece);
-        }
-        
-        board[x_direction][y_direction].setPiece(null);
+        board[finalPosition.getX_location()][finalPosition.getY_location()].setPiece(piece);
+        board[x_direction][y_direction].setPiece(null); 
+       
     }
 }
