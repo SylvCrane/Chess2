9c48cb200784bdf8c70cfe2c08d68999b9e5f9f6
SylvCrane <hng1110@autuni.ac.nz>
2022-05-24 15:53:38 +1200

    Update ChessGUIController.java

--- src/chess2/ChessGUIController.java
+++ src/chess2/ChessGUIController.java
@@ -4,10 +4,54 @@
  */
 package chess2;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author navjo
  */
-public class ChessGUIController {
+public class ChessGUIController implements ActionListener{
+    
+    ChessGUIView view;
+    
+    ChessGUIController()
+    {
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        
+        String buttonName = arg0.getActionCommand();
+        
+        if (buttonName.equals("quit"))
+        {
+            System.out.println("You pressed the quit button");
+        }
+        else
+        {
+            StringBuilder x = new StringBuilder();
+            StringBuilder y = new StringBuilder();
+            
+            StringBuilder squareName = new StringBuilder();
+            
+            int index = 0;
+            
+            x.append(buttonName.charAt(index));
+            index++;
+            y.append(buttonName.charAt(index));
+            
+            System.out.println("You pressed square[" + x.toString() + "][" + y.toString() + "]");
+          
+        }
+        
+    }
+    
+    public void addView(ChessGUIView view)
+    {
+        this.view = view;
+    }
+    
     
 }
