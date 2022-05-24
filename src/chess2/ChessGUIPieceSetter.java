05664193cb9c7ea0cf28bbfcb265b17f51b7f2b8
SylvCrane <hng1110@autuni.ac.nz>
2022-05-24 23:21:44 +1200

    Update ChessGUIPieceSetter.java

--- src/chess2/ChessGUIPieceSetter.java
+++ src/chess2/ChessGUIPieceSetter.java
@@ -6,6 +6,7 @@ package chess2;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 /**
  *
@@ -20,6 +21,102 @@ public class ChessGUIPieceSetter {
     {
         this.img = null;
         this.imgico = null;
+    }
+    
+    public void updateWhiteRook(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/whiteRook.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateWhiteKnight(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/whiteKnight.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateWhiteBishop(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/whiteBishop.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateWhiteKing(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/whiteKing.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateWhiteQueen(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/whiteQueen.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateWhitePawn(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/whitePawn.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateBlackPawn(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/blackPawn.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateBlackRook(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/blackRook.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateBlackKnight(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/blackKnight.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateBlackBishop(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/blackBishop.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateBlackKing(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/blackKing.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
+    }
+    
+    public void updateBlackQueen(JLabel pieceSquare)
+    {
+        this.img = new ImageIcon("./resources/blackQueen.png").getImage();
+        this.imgico = new ImageIcon(img);
+        pieceSquare = new JLabel();
+        pieceSquare.setIcon(imgico);
     }
 
     /**
